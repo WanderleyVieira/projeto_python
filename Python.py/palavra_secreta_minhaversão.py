84ee49a27a546af3ed4fb66449b1d6132c77cc90
Wanderley_Vieira <wanderleyfv156@gmail.com>
2026-01-31 00:07:26 -0300

    Update palavra_secreta_minhaversão.py

--- Python.py/palavra_secreta_minhaversão.py
+++ Python.py/palavra_secreta_minhaversão.py
@@ -1,58 +1,52 @@
-palavra_secreta= ''
-letras_acertadas= ''
-letras_digitadas= ''
+while True:  # jogo inteiro
+    letras_acertadas = ''
+    letras_digitadas = ''
+    palavra_secreta = ''
 
-while True:
-    while True:
+    while True:  # escolha da palavra
+        palavra_chave = input('Digite uma palavra: ')
+        confirmando_palavra = input(
+            f'Certeza que quer utilizar esta palavra: "{palavra_chave}"?\nDigite [S,s]im ou [N,n]ão '
+        )
 
-        palavra_chave= str(input('Digite uma palavra: '))
-        confirmando_palavra= input(f'Ceteza que quer utilizar está palavra: "{palavra_chave}"?\n Digite [1] SIM ou [2] NÃO')
-
-        if confirmando_palavra in ('1','Sim','SIM','sim','S','s'):
-            palavra_secreta += palavra_chave
+        if confirmando_palavra.lower() == 's':
+            palavra_secreta = palavra_chave
             print('Sua palavra foi adicionada')
-        
-        
-        elif confirmando_palavra in ('2','Não','NÃO','não','N','n'):
+            break
+        elif confirmando_palavra.lower() == 'n':
             print('Digite uma nova palavra.')
+        else:
+            print('Você precisa digitar [S,s]im ou [N,n]ão.')
+
+    while True:  # rodada do jogo
+        letra_escolhida = input('Digite uma letra: ')
+
+        if len(letra_escolhida) != 1:
+            print('Digite apenas uma letra.')
             continue
 
-        else:
-            print('Você precisa digitar [1] para Sim ou [2] para não. Voltaremos ao ínicio...')
+        if letra_escolhida in letras_digitadas:
+            print(f'Letra já digitada: {letras_digitadas}')
+            continue
 
-        while True:
+        letras_digitadas += letra_escolhida
 
-            letra_escolhida= input('Digite uma letra: ')
-            if len(letra_escolhida) == 1:
-                if letra_escolhida not in letras_digitadas:
-                    letras_digitadas += letra_escolhida
+        if letra_escolhida in palavra_secreta:
+            letras_acertadas += letra_escolhida
 
-                elif letra_escolhida in letras_digitadas:
-                    print(f'Letra já digitada: Letras Digitadas {letras_digitadas}')
+        palavra_formada = ''
+        for letra_chave in palavra_secreta:
+            if letra_chave in letras_acertadas:
+                palavra_formada += letra_chave
+            else:
+                palavra_formada += '*'
 
-                if letra_escolhida in palavra_secreta:
-                    letras_acertadas += letra_escolhida
+        print(f'Palavra formada: {palavra_formada}')
 
-                palavra_formada= ''
-                for letra_chave in palavra_secreta:
-                    if letra_chave in letras_acertadas:
-                        palavra_formada += letra_chave
-
-                    else:
-                        palavra_formada += '*'
-
-                print(f'palavra formada: {palavra_formada}')
-
-                if palavra_formada ==  palavra_secreta:
-                    print(f'parabéns palavra secreta decifrada: palavra secreta = {palavra_secreta}')
-                    print('Fim de jogo')
-
-                    palavra_formada= ''
-                    letras_acertadas= ''
-                    letras_digitadas= ''
-                    palavra_secreta= ''
-
-                    break
+        if palavra_formada == palavra_secreta:
+            print(f'\n Parabéns! Palavra secreta decifrada: {palavra_secreta}')
+            print('Reiniciando jogo...\n')
+            break 
 
 
             
