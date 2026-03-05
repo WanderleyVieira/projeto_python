7e0e129cf6c349f849135c2cb678031fc95357e9
Wanderley_Vieira <wanderleyfv156@gmail.com>
2026-03-04 21:27:06 -0300

    update fibonacci

--- Python.py/Desafio métodos cripto.py
+++ Python.py/Desafio métodos cripto.py
@@ -1,63 +1,62 @@
-import string
-import random
+import string #biblioteca onde armazena tabela ascii
+import random #biblioteca onde consigo buscar por números aleatorios através de comandos.
 
-
+#tabela ascii
 ascii1= (string.ascii_letters)
 
-print(ascii1)
-
 def pa():
-        a= random.randint(0,9)
-        r=5
+        a= random.randint(0,9) #Será sorteado um número de 0 a 9 que correspondera ao número inicial chamado a
+        r=5         #r= razão
         cripto= ''
 
         for i in palavra:
-            soma = shift + a
-            cripto += ascii1[soma % len(ascii1)]
-            a += r
+            soma = shift + a                #[soma % len(ascii1)] *assim que a tabela acabar, voltará para o inicio novamente
+            cripto += ascii1[soma % len(ascii1)] #será adicionado no cripto algumas letras da tabela ascii que corresponde ao indice do resultado da soma
+            a += r #a soma da razão com o número inicial(a), resultará no próximo número que se tornará o número inicial(a), e assim sucessivamente, até ser finalizado o for
 
-        print(cripto)
+        print(cripto) #mensagem criptografada
 
 def pg():
         a1= random.randint(0,9)
-        q=2
+        r1=2
         cripto2= ''
 
         for i in palavra:
             soma1= shift + a1
             cripto2 += ascii1[soma1 % len(ascii1)]
-            a1= a1*q
-        
+            a1= a1*r1 #a multiplicação da razão com o número inicial(a1), resultará no próximo número que se tornará o número inicial(a1),\n
+                        #e assim sucessivamente, até ser finalizado o for.
         print(cripto2)
 
 def np():
     
-    #preciso identificar uma formula para localizar os possíveis números primos.
+    #Abaixo farei uma tupla com alguns números primos.
     numeros_primos= (2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139)
-    n=0
-
+    ni=0 #ni (número do índice)
     cripto3= ''
+
     for i in palavra:
-        soma2= shift + numeros_primos[n]
+        soma2= shift + numeros_primos[ni]
         cripto3 += ascii1[soma2 % len(ascii1)]
-        n+=1
+        ni+=1
     print(cripto3)
 
 def fibonacci():
-    n=0
-    j=1
+    na=0 #número anterior
+    np=1 #número posterior
     cripto4= ''
 
     for i in palavra:
-        tabela_fibonacci= range(0,1000)
-        soma3= tabela_fibonacci[n]+ tabela_fibonacci[j]
+
+        soma3= na + np
         cripto4 += ascii1[soma3 % len(ascii1)]
-        n+=1
-        j+=1
+        na = np       #na ganha o valor de np e o np ganha o valor da soma, assim cada um pula uma casa e faz a soma, até o for se encerrar.
+        np = soma3     #ex: soma3= na(2) np(3): #na pega o valor de np= 3 e descarta o 2, np pega o valor de soma3 = 5 e atualiza seu valor, para que /
+                        # haja a soma do numero anterior (na) com o numero posterior (np), obs: valor anterior sempre é descartado.
+
+        print(soma3)
     print(cripto4)
 
-
-             
 while True:
 
 
